411523a644fd01b0b4ebd1d53003a70b4a1aea72
babayaga2703 <90399796+babayaga2703@users.noreply.github.com>
2021-09-10 07:28:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Hallo cậu',
     introDesc: 'chắc cậu bất ngờ lắm ha, đây là món quà nhỏ mà tớ làm, cùng xem nó là gì nha`,
-    btnIntro: 'tiếp tục nè',
+    btnIntro: 'Let go',
     title: 'từ lần đầu nhìn thấy cậu, tớ cực kì bị ấn tượng lun í, tiếc là chỉ thấy cậu qua fb với học online thui😔😔',
     desc: 'nhưng mà hông có sao, nhờ đó mà tớ mới có thể làm ra món quà này, xem tiếp nha ',
-    btnYes: 'Có! tớ biết đáp án của cậu mà😉',
+    btnYes: 'Có! tớ biết mà😉',
     btnNo: 'Không, mình không có hứng 🙃🙃',
     question:'cho tớ biết vì sao cậu lại chọn Có đi',
     btnReply: 'trả lời xong rùi thì gửi lại cho tớ :>>>',
-    reply: 'tớ muốn biết món quà thật sự là gì đó :333',
+    reply: 'tớ muốn biết món quà thật sự là gì:333',
     mess: 'Tớ biết mà😊😊món quà nhỏ xíu này...',
     messDesc: 'có thể giúp tớ làm quen với cậu hông 👉🏻👈🏻',
-    btnAccept: 'Okiiiii lunnn :>>>',
+    btnAccept: 'Okiiii lunn :>>>',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
